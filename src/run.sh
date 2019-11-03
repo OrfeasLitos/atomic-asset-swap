17fbc3458baa65ce62b87451a81f5a6fcc0432a6
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-11-03 20:52:25 +0000

    Automate port incrementation

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,10 @@
-./a.out 1268 1 15 &
-./a.out 1268 2 6
+SOURCE=add_fourth.oc
+UTIL=../obliv-c/test/oblivc/common/util.c
+CFLAGS="-DREMOTE_HOST=localhost -O3 -I"
+../obliv-c/bin/oblivcc ${CFLAGS} . ${SOURCE} add.c ${UTIL}
+
+PORT=`cat port`
+echo $((PORT + 1)) > port
+./a.out ${PORT} 1 15 &
+./a.out ${PORT} 2 6
+rm a.out
