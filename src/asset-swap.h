9ecac64f6d2aab39de84c1cb3536bda6f9d3ae5f
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2019-11-04 17:38:40 +0000

    Fill in ProtocolIO struct

--- src/asset-swap.h
+++ src/asset-swap.h
@@ -1,7 +1,15 @@
 #pragma once
 
 typedef struct {
-  int nothing; // TODO
+  char *secret;
+  char *secret_hash;
+
+  char *asset_plain;
+  char *asset_cipher;
+  char *asset_hash;
+
+  char *key;
+  char *key_hash;
 } ProtocolIO;
 
 void asset_swap(void *arg);
