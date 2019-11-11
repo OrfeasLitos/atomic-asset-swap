8bb50c4ae6df31bd72e5c807fda36ea506f139d5
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2019-11-11 19:26:05 +0000

    Move executable name and c source to constants

--- src/run.sh
+++ src/run.sh
@@ -1,19 +1,20 @@
 KEY_FILE=key
 ASSET_FILE=movie.mp4
 CIPHER_FILE=cipher
+EXECUTABLE=bin
 
-SOURCE=asset-swap.oc
+SOURCE="asset-swap.oc asset-swap.c"
 UTIL=../obliv-c/test/oblivc/common/util.c
 CFLAGS="-lssl -lcrypto -DREMOTE_HOST=localhost -O3 -I"
 
 python encrypt-file.py --key ${KEY_FILE} --plaintext ${ASSET_FILE} --cipher ${CIPHER_FILE}
 
-../obliv-c/bin/oblivcc ${CFLAGS} . ${SOURCE} asset-swap.c ${UTIL}
+../obliv-c/bin/oblivcc ${CFLAGS} . ${SOURCE} ${UTIL} -o ${EXECUTABLE}
 
-if [[ -f "a.out" ]]; then
+if [[ -f "${EXECUTABLE}" ]]; then
   PORT=`cat port`
   echo $((PORT + 1)) > port
-  ./a.out ${PORT} 1 ${CIPHER_FILE} &
-  ./a.out ${PORT} 2 ${ASSET_FILE}
-  rm a.out ${CIPHER_FILE}
+  ./${EXECUTABLE} ${PORT} 1 ${CIPHER_FILE} &
+  ./${EXECUTABLE} ${PORT} 2 ${ASSET_FILE}
+  rm ${EXECUTABLE} ${CIPHER_FILE}
 fi
